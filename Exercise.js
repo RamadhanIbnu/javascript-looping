5c6c47ac0b552d5f85247a8cbac9d9d114b0d2b3
RamadhanIbnu <ramadhanibnu77@gmail.com>
2020-08-16 23:10:23 +0700

    first commit

---
+++ Exercise.js
@@ -0,0 +1,41 @@
+/*
+Soal No.1
+
+Kita sudah mengetahui bahwa banyaknya user maksimal adalah 100.
+
+Buat sebuah program yang menampilkan teks ‘User ke - 1 … User ke - 100’ pada setiap baris di halaman
+HTML.
+
+Lakukan FOR LOOP pada Javascript. */
+
+for (i=1; i<=100;i++){
+    console.log("User ke- " + i);
+}
+
+/*
+Soal No.2
+
+Lakukan pengulangan menggunakan FOR LOOP untuk melakukan penambahan nilai sebanyak 10 kali.
+
+Nilai awal = 0
+
+Pengulangan = 10 kali
+
+Nilai awal ditambah 2 setiap pengulangan
+
+Tampilan hasil penambahan pada setiap pengulangan*/
+
+let nilai1=0;
+let hasil
+for (nilai1; nilai1<10;nilai1++){
+    console.log(hasil= nilai1+2);
+}
+
+/*
+Soal No.3
+Lakukan pengulangan dengan FOR LOOP yang melakukan iterasi dari 0..20.
+
+Setiap iterasi/pengulangan lakukan pengecekan apakah nilai tersebut ganjil atau genap.
+
+Tampilkan keterangan ganjil dan genap pada sebuah nilai setiap pengulangan*/
+
